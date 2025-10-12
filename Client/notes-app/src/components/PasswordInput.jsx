b85f2128b73ba653e23cade14d7edc5d97657259
0kishore007 <jkishore55555@gmail.com>
2025-10-12 12:06:11 +0530

    feat : Passwordinput component, install react icon

---
+++ Client/notes-app/src/components/PasswordInput.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import { FaRegEye, FaRegEyeSlash} from 'react-icons/fa6'
+
+const PasswordInput = ({ value, onChange, placeholder }) => {
+    const [isShowPassword, setIsShowPassword] = useState(false);
+
+    const toggleShowPassword = () => {
+        setIsShowPassword(!isShowPassword);
+    };
+
+    return (
+        <div className='flex border bg-transparent rounded px-5 mb-5 items-center'>
+            <input 
+            type={ isShowPassword ? 'text' : 'password' }
+            value={ value }
+            onChange={ onChange }
+            placeholder={ placeholder || 'Password' }
+            className='w-full outline-none py-3 text-sm bg-transparent'
+            />
+
+            { isShowPassword ? <FaRegEye
+                size={22}
+                className='text-primary cursor-pointer'
+                onClick={() => toggleShowPassword()}
+            /> : <FaRegEyeSlash 
+                size={22}
+                className='text-primary cursor-pointer'
+                onClick={() => toggleShowPassword()}    
+            />
+            }
+        </div>
+
+        
+    )
+}
+
+export default PasswordInput
--- Client/notes-app/src/pages/Login.jsx
+++ Client/notes-app/src/pages/Login.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
 import SignUp from './SignUp'
 import { Link } from 'react-router-dom'
+import PasswordInput from '../components/PasswordInput'
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // const [error, setError] = useState(null);
+
+
   return (
     <>
       <Navbar />
@@ -13,14 +20,21 @@ const Login = () => {
           <form onSubmit= { () => {} }>
             <h4 className='text-2xl mb-10 text-center'>Login</h4>
 
-            <input type='text' placeholder='Email' className='input-box mb-5' />
-            
+            <input 
+            type='text' 
+            placeholder='Email' 
+            className='input-box mb-5'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)} />
+
+            <PasswordInput value={ password }
+            onChange={(e) => setPassword(e.target.value)} />
             <p className='mb-5'><input type="checkbox" className='w-auto me-2'/>Remember Me</p>
 
             <button className='btn-primary' type='submit'>Login</button>
 
             <p className='text-sm justify-center items-center flex gap-2 mt-5'>Don't have an account?
-            <Link to='/signup' className='underline'>
+            <Link to='/signup' className='text-primary underline'>
               SignUp
             </Link> 
             </p>          
