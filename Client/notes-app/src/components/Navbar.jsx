dd001b646d11db32e2690cb0ac77e823c2625359
Kishore <jkishore55555@gmail.com>
2025-10-18 16:24:22 +0530

    feat: ProfileInfo component, helper.js

--- Client/notes-app/src/components/Navbar.jsx
+++ Client/notes-app/src/components/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import ProfileInfo from './ProfileInfo'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    navigate('/login');
+  }
+
   return (
     <div className='bg-white flex items-center justify-between py-5 px-7 drop-shadow'>
         <h1 className='text-xl font-medium text-black py-2'>Notes</h1>
+
+        <ProfileInfo onLogout={ onLogout} />
     </div>
   )
 }
---
+++ Client/notes-app/src/components/ProfileInfo.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { getInitials } from '../../utils/helper'
+
+const ProfileInfo = ({ onLogout }) => {
+  return (
+    <div className='flex gap-3 items-center'>
+        <div className='flex w-12 h-12 rounded-full font-medium bg-slate-200 text-slate-950 items-center justify-center font-medium'>
+            { getInitials("Vinsmoke Sanji") }
+        </div>
+
+        <div> 
+            <div className='text-sm font-medium'>Vinsmoke Sanji</div>
+            <button className='btn-primary rounded-md text-sm underline text-slate-100' onClick={ onLogout }>Logout</button>    
+        </div>
+    </div>
+  )
+}
+
+export default ProfileInfo
